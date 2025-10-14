4ed00c8cc506015565efb8d9d343b9a8cb6173f3
Edward Monroe <eddiemonroemedia@gmail.com>
2025-10-14 09:56:17 -0400

    more updates

--- src/components/ProjectsPortfolio.js
+++ src/components/ProjectsPortfolio.js
@@ -40,43 +40,49 @@ function ProjectsPortfolio() {
   }, []);
 
   const projects = [
-    { class: "cbd", img: cbd, name: "Carolina Bud Dispensary", link: "#" },
-    { class: "eg", img: eg, name: "Envious Gardens", link: "#" },
-    { class: "h2os", img: h2os, name: "H2O's Landscaping", link: "#" },
-    { class: "tt", img: tt, name: "Ty's Touch", link: "#" },
-    { class: "velvet", img: velvet, name: "Velvet FM Band", link: "#" },
-    { class: "velvet2", img: velvet2, name: "Velvet FM Film", link: "#" },
-    { class: "wilsons", img: wilsons, name: "Wilson's Wholesale", link: "#" },
+    { class: "cbd", img: cbd, name: "Carolina Bud Dispensary", link: "https://shopcarolinabud.com/" },
+    { class: "eg", img: eg, name: "Envious Gardens", link: "https://www.enviousgardens.com/" },
+    { class: "h2os", img: h2os, name: "H2O's Landscaping", link: "https://www.h2oslandscaping.com/" },
+    { class: "tt", img: tt, name: "Ty's Touch", link: "https://www.tystouchsc.com/" },
+    { class: "velvet", img: velvet, name: "Velvet FM Band", link: "https://velvetfmband.com/" },
+    { class: "velvet2", img: velvet2, name: "E-commerce Stores", link: "https://velvetfmband.com/apparel/" },
+    { class: "wilsons", img: wilsons, name: "Wilson's Wholesale", link: "https://wilsonswholesalecola.com/" },
   ];
 
   return (
     <section className="projects-section" id="projects">
       <div className="projects-row top">
         {projects.slice(0, 4).map((proj) => (
-          <div key={proj.name} className={`project-card ${proj.class}`}>
+          <a
+            key={proj.name}
+            href={proj.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`project-card ${proj.class}`}
+          >
             <img src={proj.img} alt={`${proj.name} Project Screenshot`} />
-            <a href={proj.link} target="_blank" rel="noopener noreferrer" className="project-btn">
-              {proj.name}
-            </a>
-          </div>
+            <div className="project-btn">{proj.name}</div>
+          </a>
         ))}
       </div>
 
       <div className="projects-row bottom">
         {projects.slice(4).map((proj) => (
-          <div key={proj.name} className={`project-card ${proj.class}`}>
+          <a
+            key={proj.name}
+            href={proj.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`project-card ${proj.class}`}
+          >
             <img src={proj.img} alt={`${proj.name} Project Screenshot`} />
-            <a href={proj.link} target="_blank" rel="noopener noreferrer" className="project-btn">
-              {proj.name}
-            </a>
-          </div>
+            <div className="project-btn">{proj.name}</div>
+          </a>
         ))}
       </div>
 
       <div className="projects-btn-wrapper">
-        <a href="/projects" className="projects-btn">
-          View Projects
-        </a>
+        <h3 className="projects-btn">Click a project to view</h3>
       </div>
     </section>
   );
